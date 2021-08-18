8851b84619bc85b5275a1069f94b1f97e79b9d62
Dzmitry Makarevich <d.a.makarevch@gmail.com>
2021-08-18 18:05:21 +0300

    Initial Commit

---
+++ AuthForm/AuthFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthFormApp.swift
+//  AuthForm
+//
+//  Created by Dzmitry Makarevich on 8/18/21.
+//
+
+import SwiftUI
+
+@main
+struct AuthFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthForm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AuthForm
+//
+//  Created by Dzmitry Makarevich on 8/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
